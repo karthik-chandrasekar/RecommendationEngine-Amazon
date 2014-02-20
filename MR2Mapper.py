681c735636b189d32e3e2115b5383569413ae55c
Karthik Chandrasekar <karthidec1@gmail.com>
2014-02-19 19:08:46 -0700

    MR2 - Generate co-occurence matrix - Unit tested

--- MR2Mapper.py
+++ MR2Mapper.py
@@ -4,7 +4,7 @@
 import sys, itertools
 
 class MR2Mapper:
-#Generate item item co-ocurrence matrix
+    #Generate item item co-ocurrence matrix
 
     def __init__(self):
         pass
@@ -16,9 +16,11 @@ class MR2Mapper:
                 if not line:continue
                 items_string = line.split("\t")[1]
                 item_string_list = eval(items_string.strip())
-                item_item_perm = itertools.permutations(item_string_list)
+                item_string_list = [item.split(":")[0] for item in item_string_list if item]
+                if len(item_string_list) == 1:continue
+                item_item_perm = itertools.permutations(set(item_string_list), 2)
                 for item_item_pair in item_item_perm:
-                    print "%s\t%s" % (item_item_pair[0], item_item_pair[1])
+                    print "%s\t%s" % (item_item_pair[0], item_item_pair[1].split(":")[0])
 
             except:
                 continue
@@ -26,3 +28,7 @@ class MR2Mapper:
 if __name__ == "__main__":
     mr2_obj = MR2Mapper()
     mr2_obj.run()
+
+#Note
+#Key - <item-id>
+#Value - <item-id>
--- MR2Reducer.py
+++ MR2Reducer.py
@@ -4,6 +4,9 @@
 import sys
 
 class MR2Reducer:
+   
+    #Generate item-item vector
+      
     def __init__(self):
         pass
 
@@ -11,41 +14,49 @@ class MR2Reducer:
         
         cur_key_item_id = None
         cur_val_item_id = None
-        pair_count  = 0
-        item_string_list = []
+        pair_count  = 1
+        item_string_set = set()
 
         for line in sys.stdin:
            
             try: 
                 if not line:continue
 
-                key_item_id, val_item_id = line.split("\t")
+                key_item_id, val_item_id = line.strip().split("\t")
                 
                 if key_item_id == cur_key_item_id or not cur_key_item_id:
                     
                     cur_key_item_id = key_item_id
                     if cur_val_item_id == val_item_id or not cur_val_item_id:
+                        if cur_val_item_id:pair_count += 1
                         cur_val_item_id = val_item_id
-                        pair_count += 1
+                        item_string_set.add("%s:%s" % (cur_val_item_id, pair_count))
 
                     else:
-                        item_string_list.append("%s\t%s:%s" % (cur_key_item_id, cur_val_item_id, pair_count))
                         cur_val_item_id = val_item_id
-                        pair_count = 0
+                        pair_count = 1
                                            
                 else:
-                    print item_string_list
+                    item_string_set.add("%s:%s" % (cur_val_item_id, pair_count))
+                    print "%s\t%s" % (cur_key_item_id, item_string_set)
                     cur_key_item_id = key_item_id
                     cur_val_item_id = val_item_id
-                    item_string_list = []
-                    pair_count = 0
+                    item_string_set = set()
+                    item_string_set.add("%s:%s" % (cur_val_item_id, pair_count))
+                    pair_count = 1
 
             except:
                 continue    
 
-        print item_string_list
+        item_string_set.add("%s:%s" % (cur_val_item_id, pair_count))
+        print "%s\t%s" % (cur_key_item_id, item_string_set)
             
 
 if __name__ == "__main__":
     mr2_obj = MR2Reducer()
     mr2_obj.run()
+
+
+#Note
+#Key - <item-id>
+#value - list of <item-id:pair_count> 
